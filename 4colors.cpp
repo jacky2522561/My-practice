d962feda35e05c37bf570d2151be2e6c4ef94802
jacky2522561 <jacky2522561@gmail.com>
2020-12-16 17:16:17 +0800

    1216_2

--- 4colors.cpp
+++ 4colors.cpp
@@ -3,64 +3,88 @@
 //double-ended-queue
 #include <deque>
 
+#define Max 26
+#define Color 4
+
 using namespace std;
 
 class GraphNode{
 	private:
 		char name;
-		//color
-
-		//color
 	public:
+		deque<int> color; //color
+		int self_color;
+		
+		//for(int i)
+		int notyet; //paint or unpaint?
 		deque<char> adj;
-		int index;
 		GraphNode(char name_){
 			name=name_;
-			index=0;
-			//color
-			
-			//color
+			notyet=1; //paint=0,unpaint=1
 		}
 		
-		showname(){
-			cout<<name<<endl;
-		}
+		/*void paint(){
+			self_color=color[i];
+			color.pop_front();
+		}*/
 		
-		/*void dump(){
-			cout<<"Vertex:"<<name<<", I'm adjacent to:";
+		void table(){
+			cout<<"Node: "<<name<<"   My neighber: ";
 			for(int i = 0; i<adj.size(); i++){
 				cout<<adj[i];
 				cout<<" ";
 			}
 			cout<<endl;
-		} */
+		}
+		
 };
 
 
 int main(){
 	deque<GraphNode*> NodeList;
-	int NodeCounter = 0;
 	char Node1,Node2;
+	
+	for(int i=0;i<Max;i++){
+		NodeList.push_back(new GraphNode((char)(i+65)));
+	}
+	
 	cout<<"(!!NOTICE!! If you want to stop input, Plz input '(z,z)')"<<endl;
 	cout<<"Plz enter the pairs of country, ex:(A,B) (B,C) etc..: "<<endl;
 	do{
 		scanf("(%c,%c)",&Node1,&Node2);
-		NodeList.push_back(new GraphNode(Node1));
-		NodeCounter++;
-		NodeList.push_back(new GraphNode(Node2));
-		NodeCounter++;
+		cout<<((int)Node1-65)<<" "<<((int)Node2-65)<<endl;
+		if(int(Node1) != 122 && int(Node2) != 122){
+			NodeList[((int)Node1-65)]->adj.push_back(Node2); 
+			NodeList[((int)Node2-65)]->adj.push_back(Node1); 
+		}
 		cout<<Node1<<Node2<<endl;
 		fflush(stdin);                  //清除上一個scanf快取 
 	}while(Node1 != 'z' && Node2 != 'z');
 	
-	delete NodeList[NodeList.size()-1];
-	delete NodeList[NodeList.size()-2];
 	
+	/*for(int i=0;i<NodeList.size();i++){
+		int color_index=1;
+		if(NodeList[i]->notyet){
+			NodeList[i]->color=color_index++;
+			for(int j=0;j<NodeList[i]->adj.size();j++){
+				NodeList[i]->adj[j]=color_index++;
+				NodeList[((int)(NodeList[i]->adj[j])-65)]->notyet=1; 
+			}
+			NodeList[i]->notyet=1; 
+		}
+		else{
+			NodeList[i]->color;
+			for(int j=0;j<NodeList[i]->adj.size();j++){
+				NodeList[i]->adj[j]=color_index++;
+				NodeList[((int)(NodeList[i]->adj[j])-65)]->notyet=1; 
+			}
+		}
+	}*/
 	
-	for(int i = 0;i<NodeList.size();i++){
-		NodeList[i]->showname();
+	int temp = ^NodeList[i]->yet;
+	for(int i=0;i<NodeList.size();i++){
+		NodeList[i]->table();
 	}
-	
 	
 	return 0;
 }
