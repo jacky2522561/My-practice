618ad378b025360c5a1b236811d7824e713056a0
jacky2522561 <jacky2522561@gmail.com>
2020-12-28 17:01:20 +0800

    1228

--- fuck.cpp
+++ fuck.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 int main(){
 	string numinput1,numinput2;
-	vector<int> numlist1,numlist2;
+	vector<int> numlist1,numlist2,sum;
 	
 	cin>>numinput1>>numinput2;
 	for(int i=0;i<numinput1.size();i++){
@@ -16,20 +16,43 @@ int main(){
 	for(int i=0;i<numinput2.size();i++){
 		numlist2.push_back(numinput2[i]-48);
 	}
+	
+	/*cout<<numinput1.size()<<endl;
+	cout<<numinput2.size()<<endl;*/
 	reverse(numlist1.begin(),numlist1.end());
 	reverse(numlist2.begin(),numlist2.end());
-	vector<int> sum
-	/*for(int i=0;i<numinput1.size();i++){
-		cout<<numlist1[i];
+	for(int i=0;i<numinput1.size()+numinput2.size();i++){
+		sum.push_back(0);
 	}
-	cout<<endl;
+	
+	/*for(int i=0;i<sum.size();i++){
+		cout<<sum[i]<<endl;
+	}*/
+	//string *sum = new string[numinput1.size()*numinput2.size()];
+	cout<< sum.size()<<endl;
+	int carry;
+	for(int i=0;i<numlist1.size();i++){
+		carry = 0;                //i=99,j=999
+		for(int j=0;j<numlist2.size();j++){
+			sum[j+i]+=(numlist1[i]*numlist2[j]+carry)%10;
+			carry=(numlist1[i]*numlist2[j]+carry)/10;
+		}		
+	}
+	for(int i=sum.size()-1;i>=0;i--){
+		cout<<sum[i];
+	}
+	/*cout<<endl;
 	for(int i=0;i<numinput2.size();i++){
 		cout<<numlist2[i];
 	}*/
-	
+	sum.clear();
 	return 0;
 } 
-no.  	 				index  0  num 24 
-       6 9 8            index  1  num 27 
-x        9 3            index  2  num 18  
---------------          
+/*no.  	 				index  0  num 24 
+       9 9 9            index  1  num 27 
+x        9 9            index  2  num 18  
+-------------- 
+     8 9 9 1
+   8 9 9 1 0
+--------------
+   9 8 9 0 1*/      
